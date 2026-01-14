36d52072c6d86d1ffb8160e68c36b4fde3bf3266
julianxrenner <julesman18@gmail.com>
2026-01-14 06:22:27 -0500

    Extra data now displays when the country is narrowed down to one

--- country-guesser-app/src/components/Display.jsx
+++ country-guesser-app/src/components/Display.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Display = ({countries}) => {
   if(countries.length === 1){
+    const image = countries[0].flags.png
     return (
-      <div>{countries[0].name.common}</div>
+      <div>
+        <p>{countries[0].name.common}</p>
+        <p>The capital is: {countries[0].capital}</p>
+        <p>Area: {countries[0].area}</p>
+        <h2>Languages</h2>
+        <h2>Flag</h2>
+        <img src={image} alt="" />
+      </div>
     )
   }else if(countries.length > 1 && countries.length < 10){
     return (
